9965d638033e79ea54c36771574caa16c8de09a0
Alam <seif.johanna@gmail.com>
2020-06-01 23:50:42 +0200

    Added example values

--- machine-learning-ex1/ex1_multi.m
+++ machine-learning-ex1/ex1_multi.m
@@ -104,8 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+X_test =  ([1650 3] - mu) ./ sigma ;
+price = [1 X_test] * theta;
 
 % ============================================================
 
@@ -149,8 +149,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+X_test = [1; 1650; 3];
+price = X_test' * theta;
 
 % ============================================================
 
