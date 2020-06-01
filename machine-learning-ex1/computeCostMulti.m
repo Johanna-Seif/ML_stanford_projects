fb2d4817c31dd146008297b85c653cf4346fa9c5
Alam <seif.johanna@gmail.com>
2020-06-01 23:33:08 +0200

    Linear regression with multiple variables

--- machine-learning-ex1/computeCostMulti.m
+++ machine-learning-ex1/computeCostMulti.m
@@ -6,16 +6,16 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+gap_X_y = X * theta - y;
+cost_vector = (gap_X_y .^ 2) / (2*m);
+J = sum(cost_vector);
 
 % =========================================================================
 
