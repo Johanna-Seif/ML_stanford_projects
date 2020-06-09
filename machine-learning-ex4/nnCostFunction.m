54ac1f2252c1066d808890579b5139b9108ecee7
Alam <seif.johanna@gmail.com>
2020-06-09 15:52:47 +0200

    Computing cost function for NN

--- machine-learning-ex4/nnCostFunction.m
+++ machine-learning-ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,21 +24,46 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
+nb_output = size(Theta2, 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the code by working through the
-%               following parts.
-%
+
 % Part 1: Feedforward the neural network and return the cost in the
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+% Adding the bias to X
+X = [ones(m, 1) X];
+% In the following each column corresponds to one data
+A2 = sigmoid(Theta1 * X'); % Computing the second second layer in vectorial
+A2 = [ones(1, m); A2]; % Adding the bias
+h_X = sigmoid(Theta2 * A2); % Computing the last second layer in vectorial
+% h_X(:, mu) is one input of the NN
+
+% Transform y into vectors,
+  % each column is the expected output for the correspondint input in X'
+y_vect = zeros(nb_output, m);
+for sample_nb = 1:m
+  expected_output = y(sample_nb);
+  y_vect(expected_output, sample_nb) = 1;
+end
+
+% Compute the cost in vectorial
+J = sum ( sum ( (-y_vect) .* log(h_X) - (1-y_vect) .* log(1-h_X) )) / m;
+
+% Regularization of the cost
+Theta1_no_bias = Theta1(:, 2:size(Theta1, 2));
+Theta2_no_bias = Theta2(:, 2:size(Theta2, 2));
+regularized_term = sum ( sum (Theta1_no_bias .^ 2) ) + ...
+                  sum ( sum (Theta2_no_bias .^ 2) );
+J = J + lambda * regularized_term / (2 * m);
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -46,12 +71,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -61,24 +86,6 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 % -------------------------------------------------------------
 
